6289131e1f1e7145655a0afc353b6b3851b374e7
Elsa Greenwalt <elsa.greenwalt@earthsoft.com>
2019-11-26 16:46:56 -0700

    I don't know what I'm doing

--- ARIMA2.py
+++ ARIMA2.py
@@ -1,10 +1,29 @@
-import pandas
-
-my_csv = pandas.read_csv('cleaned_data.csv', sep='\s*,\s*', header=0, encoding='utf-8-sig')
+import pandas as pd
+from pandas import datetime
+import matplotlib.pyplot as plt
 
 state_list = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 
+def parser(x):
+    return datetime.strptime(x, '%Y-%m')
+
 for state in state_list:
 
-    csv = my_csv[my_csv['State'] == f'{state}']
-    csv.to_csv(f'state_csvs/{state}.csv', index=False, sep=',')
+    # Create series
+    housing_prices = pd.read_csv(f'state_csvs/{state}.csv', header=0, parse_dates=[0], date_parser=parser)
+
+    # Make stationary, integrated order of 1
+    housing_prices_diff = housing_prices['Median_Listing_Price'].diff(periods=1)
+    housing_prices_diff.plot()
+    plt.show()
+
+    # Split into train/test data
+    X = housing_prices['Date'].values
+    print(X)
+    train_size = round(.8 * X.size)
+    train = X[0:train_size]
+    test = X[train_size:]
+
+    print(X.size)
+
+    print('')
